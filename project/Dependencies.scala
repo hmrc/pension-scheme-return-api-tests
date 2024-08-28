e4097b2e70bb9525ca7b579cfd38f9b30e0cffe9
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2024-08-28 16:33:17 +0100

    zap tests for jenkins

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,8 +6,8 @@ object Dependencies {
     "org.scalatest"       %% "scalatest"               % "3.2.19" % Test,
     "com.vladsch.flexmark" % "flexmark-all"            % "0.64.8" % Test,
     "com.typesafe"         % "config"                  % "1.4.3"  % Test,
-    "org.playframework"   %% "play-ahc-ws-standalone"  % "3.0.5" % Test,
+    "org.playframework"   %% "play-ahc-ws-standalone"  % "3.0.5"  % Test,
     "org.slf4j"            % "slf4j-simple"            % "2.0.13" % Test,
-    "org.playframework"   %% "play-ws-standalone-json" % "3.0.5" % Test,
+    "org.playframework"   %% "play-ws-standalone-json" % "3.0.5"  % Test
   )
 }
