97f3f9e0285466941a576da0fba6a8ccaddde4db
Vamshi-Muniganti <vamshi.muniganti@gmail.com>
2024-11-26 17:00:31 +0000

    PSR-1565-payload-changes

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ ENV=${1:-local}
 # Scalafmt checks have been separated from the test command to avoid OutOfMemoryError in Jenkins
 sbt scalafmtCheckAll scalafmtSbtCheck
 
-sbt -Denvironment=$ENV -Dsecurity.assessment=true "testOnly uk.gov.hmrc.test.api.specs.*"
+sbt -Denvironment=$ENV -Dsecurity.assessment=false "testOnly uk.gov.hmrc.test.api.specs.*"
